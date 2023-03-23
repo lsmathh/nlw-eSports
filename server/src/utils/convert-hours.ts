39b14eaa190d50d9a6371f0f1bf7c9375aefbb41
Matheus Lopes <matheuslopes@pop-os.localdomain>
2023-03-23 18:24:44 -0300

    first commit

---
+++ server/src/utils/convert-hours.ts
@@ -0,0 +1,12 @@
+
+
+
+
+export function convertHours(hourString: string){
+    const [hours, minutes] = hourString.split(':').map(Number);
+    
+
+    const minutesAmount = (hours * 60) + minutes;
+
+    return minutesAmount;
+}
